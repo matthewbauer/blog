035566d91432c84c97373a9cfc21ec9e2aa1a294
Matthew Bauer <matthew.bauer@obsidian.systems>
2018-07-26 11:29:38 -0400

    Add dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (org-static-blog-page-preamble . "<h1><a href=\"..\">Matthew Bauer</a> ◌ <a href=\"./\">Blog</a> ◌ <a href=\"./feed.xml\">RSS</a></h1>")
+  (org-static-blog-publish-title . "Matthew’s Blog")
+  (org-static-blog-publish-url "https://matthewbauer.us/blog/")
+  (org-static-blog-rss-file "feed.xml")
+  (org-static-blog-index-length 1)
+  (org-static-blog-page-header
+   "<meta name=\"author\" content=\"Matthew Bauer\">
+<meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
+<link rel=\"stylesheet\" href=\"../fonts.css\">
+<link rel=\"stylesheet\" href=\"../style.css\">")))
