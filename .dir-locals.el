0ac77743d8ae0e61ae19295fcd3ec1b52611d297
Matthew Bauer <mjbauer95@gmail.com>
2019-02-24 05:49:06 -0500

    more fixups

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,12 +2,11 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (org-static-blog-page-preamble . "<h1><a class=\"name\" href=\"..\">Matthew Bauer</a> ◌ <a href=\"./\">Blog</a> ◌ <a class=\"name\" href=\"./feed.xml\">RSS</a></h1>")
+  (org-static-blog-page-preamble . "<h1><a class=\"name\" href=\"..\">Matthew Bauer</a> ◌ <a href=\".\">Blog</a> ◌ <a class=\"name\" href=\"feed.xml\">RSS</a></h1>")
   (org-static-blog-publish-title . "Matthew’s Blog")
   (org-static-blog-publish-url . "https://matthewbauer.us/blog/")
   (org-static-blog-rss-file . "feed.xml")
   (org-static-blog-index-length . 1)
-  (org-static-blog-page-header .
-                               "  <meta name=\"author\" content=\"Matthew Bauer\">
+  (org-static-blog-page-header . "  <meta name=\"author\" content=\"Matthew Bauer\">
   <meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
   <link rel=\"stylesheet\" href=\"../style.css\">")))
