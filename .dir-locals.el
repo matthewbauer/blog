0fb4770382f675fe83f12e990b70baf193953082
Matthew Bauer <mjbauer95@gmail.com>
2019-03-22 15:06:59 -0400

    Update title

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,7 +3,7 @@
 
 ((nil
   (org-static-blog-page-preamble . "<h1><a class=\"name\" href>Matthew Bauer</a> ◌ <a href=\".\">Blog</a> ◌ <a class=\"name\" href=\"feed.xml\">RSS</a></h1>")
-  (org-static-blog-publish-title . "Matthew’s Blog")
+  (org-static-blog-publish-title . "Matthew Bauer’s Blog")
   (org-static-blog-publish-url . "https://matthewbauer.us/blog/")
   (org-static-blog-rss-file . "feed.xml")
   (org-static-blog-index-length . 1)
